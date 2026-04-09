83c25a3a77c35e706bf8c67d51ca3aca2b3ec4fa
utopiantransfer <utopiankargo@gmail.com>
2026-04-09 14:09:12 +0300

    Add files via upload
    program dosyaları yüklendi

---
+++ sw.js
@@ -0,0 +1,30 @@
+// Utopian Transfer - Service Worker v1
+const CACHE_NAME = 'utopian-transfer-v7';
+const ASSETS = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first strategy: always try fresh version, fallback to cache
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
